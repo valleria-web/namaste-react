0a50718469a5da95b726eb0b0e3b2a581f1ff327
vdsr <valleria@gmail.com>
2023-08-02 21:46:44 +0200

    filterfromvariable

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,14 @@
 import React from "react";
+import { CDN_URL } from "../utils/constants"   
 
 const Card = (props) => {
-  const { data } = props;
+    const { data } = props;
 
   return (
     <div className="card">
-      <img
-        className="card-img"
-        src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${data.cloudinaryImageId}`}
-        alt="Card Logo"
-      />
+      <img className="card-img" src= {CDN_URL + data.cloudinaryImageId}  alt="Card Logo" />
       <h3>{data.name}</h3>
+      <h4>{data.avgRating} stars</h4>
       <h4>{data.cuisines.join(", ")}</h4>
       <h4>{data.costForTwoString}</h4>
       <h4>{data.deliveryTime} minutes </h4>
