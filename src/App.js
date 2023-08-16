b26d9ab122ca40e982b640dbe9f2a8b4335c3d22
vdsr <valleria@gmail.com>
2023-08-16 17:07:25 +0200

    ¨errorpage¨

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const App = () => {
-    return (
-        <div className="app">
-            <Header />
-            <Body />
-            //Footer
-        </div>
-    );
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      //Footer
+    </div>
+  );
 };
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <App />, errorElement: <Error />},
+  { path: "/about", element: <About />,}
+]);
 
 export default App;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(<RouterProvider router={appRouter} />);
