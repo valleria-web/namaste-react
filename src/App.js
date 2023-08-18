c1cd1fb2812fe6cdd28db660b9dac92a2e02dc9f
vdsr <valleria@gmail.com>
2023-08-18 09:58:01 +0200

    router

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
       //Footer
     </div>
   );
 };
 
 const appRouter = createBrowserRouter([
-  { path: "/", element: <App />, errorElement: <Error />},
-  { path: "/about", element: <About />,}
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Body /> },
+      { path: "/about", element: <About /> },
+    ],
+    errorElement: <Error />,
+  },
 ]);
 
 export default App;
